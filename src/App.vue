ba33b504b2e99da8c6ea1bf14803ffb0a55c8fc6
giacomofigus <figusgiacomo04@gmail.com>
2024-09-23 12:13:41 +0200

    feat: imported scss and all files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup>
+<script>
+  import './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
 
+  export default{
+    components: {
+      AppHeader,
+    }
+  }
 </script>
 
 <template>
-
+  <AppHeader/>
+  
 </template>
 
 <style scoped lang="scss">
+  @use './assets/styles/general.scss' as *;
 
+  
 </style>
